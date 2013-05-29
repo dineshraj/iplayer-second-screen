ae05e9ed760a543d90c335b228e215b4e7d44228
D Goomany <dineshraj.goomany@bbc.co.uk>
2013-05-29 16:59:32 +0100

    * Removed all reference to history and username * Added code to pass on PID from the server to clients.

--- server/iplayer-second-screen.js
+++ server/iplayer-second-screen.js
@@ -14,8 +14,6 @@ var http = require('http');
 /**
  * Global variables
  */
-// latest 100 messages
-var history = [ ];
 // list of currently connected clients (users)
 var clients = [ ];
 
@@ -48,59 +46,30 @@ var wsServer = new webSocketServer({
 // This callback function is called every time someone
 // tries to connect to the WebSocket server
 wsServer.on('request', function(request) {
-    console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
+//  console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
 
-    // accept connection - you should check 'request.origin' to make sure that
-    // client is connecting from your website
-    // (http://en.wikipedia.org/wiki/Same_origin_policy)
     var connection = request.accept(null, request.origin); 
     // we need to know client index to remove them on 'close' event
     var index = clients.push(connection) - 1;
-    var userName = false;
 
-    console.log((new Date()) + ' Connection accepted.');
+//  console.log((new Date()) + ' Connection accepted.');
 
-    // send back chat history
-    if (history.length > 0) {
-        connection.sendUTF(JSON.stringify( { type: 'history', data: history} ));
-    }
 
     // user sent some message
     connection.on('message', function(message) {
-        if (message.type === 'utf8') { // accept only text
-            if (userName === false) { // first message sent by user is their name
-                // remember user name
-                userName = htmlEntities(message.utf8Data);
-                console.log((new Date()) + ' User is known as: ' + userName);
-
-            } else { // log and broadcast the message
-                console.log((new Date()) + ' Received Message from ' + userName + ': ' + message.utf8Data);
-                
-                // we want to keep history of all sent messages
-                var obj = {
-                    time: (new Date()).getTime(),
-                    text: htmlEntities(message.utf8Data),
-                    author: userName
-                };
-                history.push(obj);
-                history = history.slice(-100);
-
-                // broadcast message to all connected clients
-                var json = JSON.stringify({ type:'message', data: obj });
-                for (var i=0; i < clients.length; i++) {
-                    clients[i].sendUTF(json);
-                }
-            }
+        if (message.type === 'pid') {
+            /*
+             * a client has sent a PID to the server we have to
+             * broadcast it again for it to reach the other clients
+             */
+            connection.send(message);
         }
     });
 
     // user disconnected
     connection.on('close', function(connection) {
-        if (userName !== false) {
-            console.log((new Date()) + " Peer " + connection.remoteAddress + " disconnected.");
-            // remove user from the list of connected clients
-            clients.splice(index, 1);
-        }
+        // remove user from the list of connected clients
+        clients.splice(index, 1);
     });
 
 });
