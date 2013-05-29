304651493dbdb19fdc1396793634af78813307f3
D Goomany <dineshraj.goomany@bbc.co.uk>
2013-05-29 17:31:41 +0100

    * Send message to each client fix

--- server/iplayer-second-screen.js
+++ server/iplayer-second-screen.js
@@ -60,7 +60,11 @@ wsServer.on('request', function(request) {
              * a client has sent a message to the server we have to
              * broadcast it again for it to reach the other clients
              */
-            connection.send(message);
+
+            var json = JSON.stringify(message);
+                for (var i = 0; i < clients.length; i++) {
+                    clients[i].sendUTF(json);
+            }
     });
 
     // user disconnected
